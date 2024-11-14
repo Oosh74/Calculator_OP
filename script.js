9df0ab0bb786477395565da00078240a79610fd9
Oosh74 <usherjared@yahoo.com>
2024-11-13 20:48:32 -0500

    Add clear button, changed how text display is handled

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let numberButtons = document.querySelectorAll('.number-button');
 let numbers = document.querySelector('.numbers');
+let clearBtn = document.querySelector('.clear-button');
+let inputText = [];
 
 numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    numbers.textContent += button.textContent;
+    inputText.push(button.textContent);
+    numbers.textContent = inputText.join('');
   });
+});
+
+clearBtn.addEventListener('click', () => {
+  inputText = [];
+  numbers.textContent = 0;
 });
