c1a0aba418aed71da764d2df66eb4019bd30e991
Oosh74 <usherjared@yahoo.com>
2024-11-13 20:58:39 -0500

    Operator buttons connected to JS and display in the text area on the calculator

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let numberButtons = document.querySelectorAll('.number-button');
 let numbers = document.querySelector('.numbers');
 let clearBtn = document.querySelector('.clear-button');
+let operators = document.querySelectorAll('.operator');
+
 let inputText = [];
 
 numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    inputText.push(button.textContent);
+    numbers.textContent = inputText.join('');
+  });
+});
+
+operators.forEach((button) => {
   button.addEventListener('click', () => {
     inputText.push(button.textContent);
     numbers.textContent = inputText.join('');
