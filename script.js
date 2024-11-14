ef103111e5c7b60676addd663936a170656431bc
Oosh74 <usherjared@yahoo.com>
2024-11-13 22:01:55 -0500

    Add basic math functionality and equate, solution updates in text area

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-let numberButtons = document.querySelectorAll('.number-button');
-let numbers = document.querySelector('.numbers');
-let clearBtn = document.querySelector('.clear-button');
-let operators = document.querySelectorAll('.operator');
+const numberButtons = document.querySelectorAll('.number-button');
+const numbers = document.querySelector('.numbers');
+const clearBtn = document.querySelector('.clear-button');
+const operators = document.querySelectorAll('.operator');
+const equalSign = document.querySelector('.equal-sign');
 
 let inputText = [];
+let x = '';
+let y = '';
+let operator = '';
+
+let operatorObj = {
+  '+': (x, y) => {
+    return Number(x) + Number(y);
+  },
+  '-': (x, y) => {
+    return Number(x) - Number(y);
+  },
+  '/': (x, y) => {
+    return Number(x) / Number(y);
+  },
+  '*': (x, y) => {
+    return Number(x) * Number(y);
+  },
+};
 
 numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
     inputText.push(button.textContent);
     numbers.textContent = inputText.join('');
+    if (operator.length === 0) {
+      x += button.textContent;
+    } else {
+      y += button.textContent;
+    }
   });
 });
 
@@ -16,10 +40,34 @@ operators.forEach((button) => {
   button.addEventListener('click', () => {
     inputText.push(button.textContent);
     numbers.textContent = inputText.join('');
+    operator = button.textContent;
   });
 });
 
 clearBtn.addEventListener('click', () => {
   inputText = [];
   numbers.textContent = 0;
+  x = '';
+  y = '';
+  operator = '';
 });
+
+equalSign.addEventListener('click', () => {
+  numbers.textContent = `${operatorObj[operator](x, y)}`;
+});
+
+// const add = (x, y) => {
+//   return x + y;
+// };
+
+// const subtract = (x, y) => {
+//   return x - y;
+// };
+
+// const divide = (x, y) => {
+//   return x / y;
+// };
+
+// const multiple = (x, y) => {
+//   return x * y;
+// };
